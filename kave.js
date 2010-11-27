10a552fc62a8921e85d6f9bd4632a06247ec2412
skilldrick <skilldrick@gmail.com>
2010-11-27 21:55:33 +0000

    Started on the walls and snowball.

--- kave.js
+++ kave.js
@@ -3,29 +3,73 @@
 * a: context
 */
 var counter = 0;
-var width = 400;
-var height = 400;
+/** @const */ var width = 400;
+/** @const */ var height = 400;
+/** @const */ var white = 'white';
+var snowballX = 20;
+var snowballY = 50;
+var gameOver = false;
+var rand = Math.random;
+
 c.width = width;
 c.height = height;
 
 function gameloop() {
-  a.fillStyle = 'black';
+  a.fillStyle = 'chocolate';
   a.fillRect(0,0,width,height);
   counter++;
+  icicles();
+  walls(); 
+  //as long as these happen in the right order, only need to set a.fillStyle = white once
+  snowball();
   snow();
-  setTimeout(gameloop, 50);
+  if(!gameOver) {
+    setTimeout(gameloop, 50);
+  }
+  else {
+    //write game over?
+  }
+}
+
+function detectCollision() {
+  if (a.isPointInPath(snowballX, snowballY)) {
+    gameOver = true;
+    console.log(snowballX, snowballY);
+  }
+}
+
+function icicles() {
+
+}
+
+var wallPoints = [5, 20, 8, 40];
+function walls() {
+  a.fillStyle = white;
+  a.beginPath();
+  a.lineTo(0,0);
+  for (var i = 0; i < wallPoints.length; i++) {
+    a.lineTo(i * 50, wallPoints[i]);
+  }
+  a.lineTo(width + 100, 0);
+  detectCollision();
+  a.fill();
+}
+
+function snowball() {
+  a.beginPath();
+  a.arc(snowballX, snowballY, 10, 0, Math.PI*2);
+  a.fill();
 }
 
 var flakes = 978;
 var snowArray = [];
 for(var j = 0; j < flakes; j++) {
-  snowArray.push(width*height/flakes * j + Math.random() * 100);
+  snowArray.push(width*height/flakes * j + rand() * 100);
 }
 
 function snow() {
-  a.fillStyle = 'rgba(255,255,255,0.8)';
   for(var j = 0; j < flakes; j++) {
-    var s = snowArray[j] + width + Math.random() - 0.5 + Math.sin(i/4) / 4;
+    var s = snowArray[j] + width + rand() - 0.5 + Math.sin(counter/4) / 4;
     s %= width*height;
     snowArray[j] = s;
     a.fillRect(snowArray[j] % width, snowArray[j] / width, 1, 1);
@@ -33,4 +77,3 @@ function snow() {
 }
 
 gameloop();
-
